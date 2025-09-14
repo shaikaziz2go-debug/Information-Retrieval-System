db55c3a5631a66f8d8df152f224198158c6c2f27
Aziz <shaikaziz2go@gmail.com>
2025-09-13 20:16:07 -0400

    Add initial setup and requirements files for the Generative AI Project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aziz',
+    author_email= 'shaikaziz2go@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
